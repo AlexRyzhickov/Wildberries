3a22c6045b7af686bd0e3690345a866de7bad6fa
AlexRyzhickov <makseljoinb@gmail.com>
2022-09-29 09:12:11 +0300

    adding task_2

---
+++ L2/task_2/go.mod
@@ -0,0 +1,5 @@
+module Wildberries/L2/task_2
+
+go 1.16
+
+require github.com/stretchr/testify v1.8.0
---
+++ L2/task_2/main_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestUnpackString(t *testing.T) {
+	unpackString("")
+	assert.Equal(t, unpackString("я4яcяd5e"), "яяяяяcяddddde", "they should be equal")
+	assert.Equal(t, unpackString("a1b5c2d5e11"), "abbbbbccdddddeeeeeeeeeee", "they should be equal")
+	assert.Equal(t, unpackString("a1b5c2d5e"), "abbbbbccddddde", "they should be equal")
+	assert.Equal(t, unpackString("a2d3b4"), "aadddbbbb", "they should be equal")
+	assert.Equal(t, unpackString("a4"), "aaaa", "they should be equal")
+	assert.Equal(t, unpackString("a"), "a", "they should be equal")
+	assert.Equal(t, unpackString("4a"), "a", "they should be equal")
+	assert.Equal(t, unpackString("45"), "", "they should be equal")
+	assert.Equal(t, unpackString("4"), "", "they should be equal")
+	assert.Equal(t, unpackString(""), "", "they should be equal")
+}
